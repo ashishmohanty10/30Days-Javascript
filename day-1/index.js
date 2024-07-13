ae0f6a0b247c7f2dc98c9d9d3b7b48f4965430f5
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-07-13 20:06:25 +0530

    first-day-done

---
+++ day-1/index.js
@@ -0,0 +1,32 @@
+// Task 1
+var a = 23;
+console.log(a);
+
+// Task 2
+let b = "ashish";
+console.log(b);
+
+// Task 3
+const bool = false;
+console.log(bool);
+
+//Task 4
+let x = 10;
+let y = "ashish";
+let z = {
+  1: "id1",
+};
+let p = true;
+let q = [1, 2, 3, 4, 5];
+console.log(x, y, z, p, q);
+
+//Task 5
+let c = 23;
+c = 34;
+console.log(c);
+
+//Task 6
+const ar = 23;
+ar = 45;
+
+console.log(ar);
