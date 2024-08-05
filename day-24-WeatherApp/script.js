6827fd35207597a82d05559083d5462fd55e192e
ASHISH MOHANTY <ashishmohaty10@gmail.com>
2024-08-05 21:26:06 +0530

    day-24 weather done

---
+++ day-24-WeatherApp/script.js
@@ -0,0 +1,26 @@
+const apiKey = "d54b93f5e3e4fdc2a82260e66ca0f1a1";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+document.getElementById("searchButton").addEventListener("click", () => {
+  const location = document.getElementById("locationInput").value;
+  if (location) {
+    fetchWeather(location);
+  }
+});
+
+function fetchWeather(location) {
+  const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("location").textContent = data.name;
+      document.getElementById("temperature").textContent = `${Math.round(
+        data.main.temp
+      )}°C`;
+      document.getElementById("description").textContent =
+        data.weather[0].description;
+    })
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+    });
+}
